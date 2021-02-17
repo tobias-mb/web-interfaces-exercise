5323903478e8e2fd519c1f246223bddcf876323a
tobias-mb <tobias.mb@gmx.de>
2021-02-17 14:25:43 +0100

    small update to login route

--- server/routes/login.js
+++ server/routes/login.js
@@ -8,7 +8,11 @@ router.post('/', passport.authenticate('basic', {session : false}), (req, res) =
     console.log("successful log in for user:")
     console.log(req.user);
     res.status(200);
-    res.json({id: req.user.id})
+    res.json({
+      id: req.user.id,
+      username: req.user.username,
+      email: req.user.email
+    })
   })
 
 module.exports = router;
