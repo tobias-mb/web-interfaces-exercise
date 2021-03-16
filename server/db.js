3ec3c76cb0bb53e252a5076f5119a7fcd23e5c05
tobias-mb <tobias.mb@gmx.de>
2021-03-16 14:42:43 +0100

    ssl fix for heroku

--- server/db.js
+++ server/db.js
@@ -13,3 +13,20 @@ try {
 
 
 module.exports = pool;
+
+/* This works for new heroku update
+
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
+module.exports = client;
+  
+ */
